7293a5565c113448698b4cfca8d9811bd109d2f7
Sonia R <Sonia R>
2017-04-08 15:58:45 +0200

    new

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var i;
+function DrawTree (i) {
+
+	for ( i = 0 ; i = 6 ; i++ ) {
+		var star = '';
+		for ( var j = 0 ; j = i ; j++) {
+			star += '*'
+		}
+		console.log(star);
+
+	}
+}
+DrawTree (1);
+DrawTree (6);
