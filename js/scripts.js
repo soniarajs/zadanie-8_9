d5196e8f8f4f25834f7c17c1211a64a31f0de783
Sonia R <Sonia R>
2017-04-18 23:33:07 +0200

    popr

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
-var i;
-function DrawTree (i) {
+function DrawTree (rows) {
+	var star
 
-	for ( i = 0 ; i = 6 ; i++ ) {
-		var star = '';
-		for ( var j = 0 ; j = i ; j++) {
+	for ( i = 1 ; i <= rows ; i++ ) {
+	star = '';
+		for ( var j = 0 ; j < i ; j++) {
 			star += '*'
 		}
 		console.log(star);
 
 	}
 }
-DrawTree (1);
-DrawTree (6);
+DrawTree (4);
